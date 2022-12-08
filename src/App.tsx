16c4ab85b387ce1ab58ce7ff4479fa4584679d7d
luoguan <2256477577@qq.com>
2022-12-08 09:19:48 +0800

    feat:基础组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Button } from './components/button';
 
 const App = (props) => {
   return (<>
+    <Button type="link" href='#000'>test</Button>
+    <Button type='primary' size='large'>test</Button>
     <p>jchsdjghvcjkdfsgvgdfkjgvjdkg</p>
   </>)
 }
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,7 +1,33 @@
 import React, { FC } from 'react';
+import cx from 'classnames'
 
-export const Button: FC<{}> = () => {
-  return (<div>
-    9090909
-  </div>)
+type IBtnSize = 'small' | 'large'
+
+type IBtnType = 'link' | 'primary' | 'danger'
+
+interface IBtnProps {
+  type?: IBtnType;
+  size?: IBtnSize;
+  disabled?: boolean;
+  className?: string;
+  href?: string;
+  children?: React.ReactNode;
+}
+
+export const Button: FC<IBtnProps> = (props) => {
+  const { type, size, disabled, className, children, href } = props
+  const classes = cx({
+    [`btn-${type}`]: type,
+    [`btn-${size}`]: size,
+    className
+  })
+  if (type === 'link' && href) {
+    return <a className={classes} href={href}>{ children }</a>
+  }
+   return (<button
+     className={classes}
+     disabled
+  >
+    {children}
+  </button>)
 }
